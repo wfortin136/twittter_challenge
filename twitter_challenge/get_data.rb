433e76fa08fb900a6e2418b220d437cf2ac9fcfe
B410 <wjf136@gmail.com>
2015-12-14 01:13:32 -0600

    Updated with json backup file during runtime

--- twitter_challenge/get_data.rb
+++ twitter_challenge/get_data.rb
@@ -1,6 +1,25 @@
 require 'rubygems'
 require 'tweetstream'
 
+# save ruby object as json
+def save_json(object, name)
+  object_json = object.to_json
+  File.open(name+".json", "w") do |f|
+    f.write(object_json)
+  end
+end
+
+time = ARGV[0].to_i
+if ARGV[1]
+  existing_file_count = ARGV[1]
+  file = File.open(ARGV[1], "r+").read
+  word_count = JSON.parse(file)    # parse json to ruby object
+else
+  word_count = Hash.new
+end
+
+stop_words = File.open("stop-word-list.txt", "r").read
+
 TweetStream.configure do |config|
     config.consumer_key       = 'PEF5yBs0mt99PrzgwTPr61Ahe'
     config.consumer_secret    = 'NGFIHSCf4Psdc6jKuoWEixcITcTAbAgdekdgJOdLqHCJEbtZYG'
@@ -10,9 +29,8 @@ TweetStream.configure do |config|
 end
 
 client = TweetStream::Client.new
-tweets = []
 errors = []
-word_count = Hash.new
+tweets = []
 
 client.on_error do |error|
   puts "#{error.text}"
@@ -21,33 +39,45 @@ end
 
 tweets_thr = Thread.new{
 
-  puts " Thread 2"
+  #puts " Thread 2"
   client.sample do |tweet|
-    # The status object is a special Hash with
-    # method access to its keys.
-    #puts "#{status.text}"
     tweets.push(tweet)
   end
 }
 
 parse_thr = Thread.new{
-  puts "Thread 3"
+  #puts "Thread 3"
   while (tweets_thr.status || tweets.length>0)
     if tweets.length>0
       t = tweets.shift
-      #puts "#{t.text}"
       words = t.text.split
       words.each do |w|
-        if word_count.key?(w)
-          word_count[w] = word_count[w] + 1
-        else
-          word_count[w]= 1
+        if !(stop_words.include? w.downcase)
+          if word_count.key?(w)
+            word_count[w] = word_count[w] + 1
+          else
+            word_count[w]= 1
+          end
         end
       end
+      save_json(word_count, "word_count_backup")
+      system 'mv word_count_backup.json word_count.json'
     end
   end
 }
-sleep(5)
+
+puts "Start"
+for i in 1..time
+  delim = 5
+  count = (60/delim).to_i
+  for x in 1..count
+    sleep(delim)
+    print "."
+    #puts x*delim
+  end
+  puts "#{i} Minute(s)"
+end
+
 Thread.kill(tweets_thr)
 parse_thr.join
 
